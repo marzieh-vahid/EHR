af3c8c689194478dbedff16e34d2cc69f0b34c4e
marzieh.vahid <marzieh.vahid@gmail.com>
2013-05-13 17:55:19 +0430

    Initial commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EHR::Application.config.secret_token = '788200646453714877f356b2572a8d3bbfced10002b43b271e8b3cbc14364496ba0f255e43dddab23bad572110109fe6ab570ad16a799344973bf0bc966d8c6c'
