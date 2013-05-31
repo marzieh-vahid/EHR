e6e65ccfdfcee4ec4bf53f2ce1742a575fe285ae
marzieh.vahid <marzieh.vahid@gmail.com>
2013-06-01 00:51:51 +0430

    Finish doctor signup

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-EHR::Application.initialize!
+
+ActiveSupport::Deprecation.silenced = true EHR::Application.initialize!
